d79346e0f712dda1bb9405e14ba9249d5c485d6c
James C Russell <james@101ideas.cz>
2017-06-18 22:33:01 +0200

    Prompt.

---
+++ lib/refined-refinements/cli/prompt.rb
@@ -0,0 +1,107 @@
+module RR
+  class InvalidResponse < StandardError; end
+
+  class Answer
+    def initialize
+      @callbacks = Hash.new { Proc.new { true } }
+    end
+
+    def validate_raw_value(regexp, allow_empty: nil)
+      @callbacks[:validate_raw_value] = Proc.new do |raw_value|
+        unless (allow_empty && raw_value.empty?) || raw_value.match(regexp)
+          raise InvalidResponse.new("Doesn't match #{regexp}.")
+        end
+      end
+    end
+
+    def validate_clean_value(&block)
+      @callbacks[:validate_clean_value] = block
+    end
+
+    def clean_value(&block)
+      @callbacks[:get_clean_value] = block
+    end
+
+    def run(raw_value)
+      @callbacks[:validate_raw_value].call(raw_value)
+      clean_value = @callbacks[:get_clean_value].call(raw_value)
+
+      unless @callbacks[:validate_clean_value].call(clean_value)
+        raise InvalidResponse.new
+      end
+
+      clean_value
+    end
+
+    def self_or_retrieve_by_index(list, raw_value, default_value = nil)
+      if raw_value.match(/^\d+$/)
+        list[raw_value.to_i - 1]
+      elsif raw_value.empty?
+        default_value
+      else
+        raw_value
+      end
+    end
+
+    def retrieve_by_index_or_self_if_on_the_list(list, raw_value, default_value = nil)
+      if raw_value.match(/^\d+$/)
+        list[raw_value.to_i - 1]
+      elsif list.include?(raw_value)
+        raw_value
+      elsif raw_value.empty? && default_value
+        default_value
+      else
+        raise InvalidResponse.new(raw_value)
+      end
+    end
+
+    def convert_money_to_cents(raw_value)
+      if raw_value.match(/\./)
+        raw_value.delete('.').to_i
+      else
+        "#{raw_value}00".to_i
+      end
+    end
+  end
+
+  class Prompt
+    using RR::ColourExts
+
+    attr_reader :data
+    def initialize
+      @data = Hash.new
+    end
+
+    # options: ['one', 'two']
+    # help: 'One of one, two or any integer value.'
+    def prompt(key, prompt_text, **options, &block)
+      answer = Answer.new
+      answer.instance_eval(&block)
+
+      help = help(**options)
+      print "<bold>#{prompt_text}</bold>#{" (#{help})" if help}: ".colourise
+      @data[key] = answer.run(STDIN.readline.chomp)
+      raise InvalidResponse.new if @data[key].nil? && options[:required]
+      @data[key]
+    rescue InvalidResponse => error
+      puts "<red>Invalid response</red>, try again.".colourise
+      retry
+    end
+
+    # currency_help = " (#{self.show_label_for_self_or_retrieve_by_index(currencies)})" unless currencies.empty?
+    # print "Currency#{currency_help}: "
+    # expense_data[:currency] = self.self_or_retrieve_by_index(currencies, STDIN.readline.chomp, 'EUR')
+    # TODO: Say that it defaults to EUR.
+    # TODO: raw_value = 'EUR' vs. default: 'EUR' ???
+    # TODO: If it evals as nil, shall we still add it to the data?
+    def help(help: nil, options: Array.new, default: nil, **rest)
+      if help then help
+      elsif help.nil? && ! options.empty?
+        options = options.map.with_index { |key, index|
+          "<green>#{key}</green> <magenta>#{index + 1}</magenta>"
+        }.join(' ').colourise
+        default ? "#{options}; defaults to #{default}" : options
+      else end
+    end
+  end
+end
