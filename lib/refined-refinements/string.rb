4034c20ee9c6953f8fa5434d8ee65860f16509e6
James C Russell <james@101ideas.cz>
2017-07-31 09:16:24 +0200

    Array#join_with_and

--- lib/refined-refinements/string.rb
+++ lib/refined-refinements/string.rb
@@ -5,5 +5,13 @@ module RR
         "#{self[0].upcase}#{self[1..-1]}"
       end
     end
+
+    refine Array do
+      def join_with_and(xxx = 'and', delimiter = ', ', &block)
+        block = Proc.new { |item| item } if block.nil?
+        return block.call(self[0]) if self.length == 1
+        "#{self[0..-2].map(&block).join(delimiter)} #{xxx} #{block.call(self[-1])}"
+      end
+    end
   end
 end
