eb1bd9c5412e84b6126baee0d9a15cc3a154a421
James C Russell <james@101ideas.cz>
2017-08-02 22:21:51 +0200

    Automated commit from 02/08/2017.

--- lib/refined-refinements/cli/prompt.rb
+++ lib/refined-refinements/cli/prompt.rb
@@ -1,19 +1,3 @@
-# def prompt_date
-#   help = "<green>#{Date.today.iso8601}</green>, anything that <magenta>Data.parse</magenta> parses or <magenta>-1</magenta> for yesterday etc"
-#   @prompt.prompt(:date, 'Date', help: help) do
-#     clean_value do |raw_value|
-#       case raw_value
-#       when /^$/ then Date.today
-#       when /^-(\d+)$/ then Date.today - $1.to_i
-#       else Date.parse(raw_value) end
-#     end
-#
-#     validate_clean_value do |clean_value|
-#       clean_value.is_a?(Date)
-#     end
-#   end
-# end
-#
 # def prompt_type
 #   @prompt.prompt(:type, 'Type', options: Expense::TYPES) do
 #     clean_value do |raw_value|
@@ -22,46 +6,6 @@
 #
 #     validate_clean_value do |clean_value|
 #       Expense::TYPES.include?(clean_value)
-#     end
-#   end
-# end
-#
-# def prompt_tip
-#   @prompt.prompt(:tip, 'Tip') do
-#     validate_raw_value(/^\d+(\.\d{2})?$/, allow_empty: true)
-#
-#     clean_value do |raw_value|
-#       convert_money_to_cents(raw_value)
-#     end
-#   end
-# end
-#
-# def prompt_currency(expenses)
-#   currencies = expenses.map(&:currency).uniq
-#   @prompt.prompt(:currency, 'Currency code', options: currencies, default: expenses.last.currency) do
-#     clean_value do |raw_value|
-#       (not raw_value.empty?) ? self.self_or_retrieve_by_index(currencies, raw_value) : expenses.last.currency
-#     end
-#
-#     validate_clean_value do |clean_value|
-#       clean_value.match(/^[A-Z]{3}$/)
-#     end
-#   end
-# end
-#
-# def prompt_location(expenses)
-#   locations = expenses.map(&:location).uniq.sort_by { |location|
-#     puts "#{location}: #{expenses.count { |expense| expense.location == location }}"
-#     expenses.count { |expense| expense.location == location }
-#   }.reverse
-#
-#   @prompt.prompt(:location, 'Location', options: locations, default: expenses.last.location) do
-#     clean_value do |raw_value|
-#       (not raw_value.empty?) ? self.self_or_retrieve_by_index(locations, raw_value) : expenses.last.location
-#     end
-#
-#     validate_clean_value do |clean_value|
-#       clean_value && ! clean_value.empty?
 #     end
 #   end
 # end
