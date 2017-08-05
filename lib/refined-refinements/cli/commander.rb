329e0fe5197689a29ae61e1cb5936e36b0d8843a
James C Russell <james@101ideas.cz>
2017-08-05 09:42:38 +0200

    WIP

---
+++ lib/refined-refinements/cli/commander.rb
@@ -0,0 +1,57 @@
+require 'refined-refinements/string'
+require 'refined-refinements/colours'
+
+module RR
+  class Commander
+    using RR::ColourExts
+
+    def self.commands
+      @commands ||= Hash.new
+    end
+
+    def self.command(command_name, command_class)
+      self.commands[command_name] = command_class
+    end
+
+    def help_template(program_name)
+      <<-EOF
+<red.bold>:: #{program_name} ::</red.bold>
+
+<cyan.bold>Commands</cyan.bold>
+      EOF
+    end
+
+    def help
+      self.commands.reduce(self.help_template) do |buffer, (command_name, command_class)|
+        command_help = command_class.help.split("\n").map { |line| line.sub(/^\s*/, '') }.join("\n")
+        command_class.help ? [buffer, command_help].join("\n") : buffer
+      end.colourise
+    end
+
+    def commands
+      self.class.commands
+    end
+
+    def run(command_name, args)
+      command_class = self.class.commands[command_name]
+      command = command_class.new(self.command_instantiation_arguments(args))
+      command.run
+    end
+  end
+
+  class Command
+    using RR::ColourExts
+    using RR::StringExts # #titlecase
+
+    class << self
+      attr_accessor :help
+      def main_command
+        File.basename($0)
+      end
+    end
+
+    def initialize(args)
+      @args = args
+    end
+  end
+end
---
+++ lib/refined-refinements/collection.rb
@@ -0,0 +1,150 @@
+require 'yaml'
+require 'forwardable'
+require 'pathname'
+
+# flashcards = Collection.new(Flashcard, 'es.yml')
+# flashcards << flashcard
+# flashcards.save
+module RR
+  class Collection
+    def self.data_file_dir
+      Pathname.new("~/Dropbox/Data/Data/Flashcards").expand_path
+    end
+
+    # def initialize(item_class, basename)
+    #   @path = self.class.data_file_dir.join("#{basename}.yml")
+    #   @item_class, @activity_filters = item_class, Hash.new
+    # end
+    def initialize(basename)
+      @path = self.class.data_file_dir.join(basename)
+      @activity_filters = Hash.new
+    end
+
+    def items(&block)
+      @items ||= self.load_raw_collection.map do |data|
+        begin
+          block.call(data)
+        rescue => error
+          abort "Loading item #{data.inspect} failed: #{error.message}.\n\n#{error.backtrace}"
+        end
+      end
+    end
+
+    def active_items
+      return self.items if @activity_filters.empty?
+
+      @activity_filters.keys.reduce(self.items) do |items, filter_name|
+        self.run_filter(filter_name, items)
+      end
+    end
+
+    def run_filter(filter_name, items)
+      items.select { |item| @activity_filters[filter_name].call(item) }
+    end
+
+    def filter(filter_name, &block)
+      @activity_filters[filter_name] = block if block
+      self
+    end
+
+    def filter_out(filter_name, &block)
+      @activity_filters[filter_name] = Proc.new { |item| ! block.call(item) } if block
+      self
+    end
+
+    def filters
+      @activity_filters.keys
+    end
+
+    def has_filter?(filter_name)
+      @activity_filters.has_key?(filter_name)
+    end
+
+    def filtered_out_items(filter_name)
+      self.items - self.run_filter(filter_name, self.items)
+    end
+
+    # flashcards[:expressions, 'hacer']
+    # flashcards[:translations, :silent_translations, 'to be']
+    def [](key, value)
+      self.items.select do |item|
+        [item.send(key)].flatten.include?(value)
+      end
+    end
+
+    extend Forwardable
+
+    # Generally we want to avoid proxying methods, unless
+    # it is clear whether they would be performed on items or active_items.
+    def_delegator :items, :<<
+
+    def replace(original_item, new_item)
+      index = self.items.index(original_item)
+      self.items.delete(original_item)
+      self.items.insert(index, new_item)
+    end
+
+    def save
+      return if self.items.empty?
+
+      if File.mtime(@path.to_s) > @loaded_at
+        raise "Cannot be saved #{File.mtime(@path.to_s).inspect} vs. #{@loaded_at.inspect}"
+      end
+
+      self.save_to(@path) && self.save_to(self.back_up_path)
+
+      @loaded_at = Time.now # Otherwise the next save call with fail.
+      true
+    end
+
+    def save_to(path)
+      updated_data = self.serialise
+      if (! File.exist?(path)) || File.read(path) != updated_data
+        path.open('w') do |file|
+          file.write(updated_data)
+        end
+        return true
+      else
+        return false
+      end
+    end
+
+    def back_up_path
+      chunks    = @path.basename.to_s.split('.')
+      timestamp = Time.now.strftime('%Y-%m-%d-%H-%M')
+      basename  = chunks.insert(-2, timestamp).join('.')
+
+      self.class.data_file_dir.join('Backups', basename)
+    end
+
+    # TODO: Should we deprecate this?
+    # It's best to use explicit .items/.active_items calls.
+    include Enumerable
+
+    def each(&block)
+      if block
+        self.active_items.each(&block)
+      else
+        self.active_items.to_enum
+      end
+    end
+
+    protected
+    def load_raw_collection
+      @loaded_at = Time.now
+
+      self.deserialise(@path.to_s)
+    rescue Errno::ENOENT
+      Array.new
+    end
+
+    def deserialise(path)
+      # YAML treats an empty string as false.
+      YAML.load_file(path) || Array.new
+    end
+
+    def serialise
+      self.items.map(&:data).to_yaml
+    end
+  end
+end
